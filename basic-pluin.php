98c78ac33c5e27f7c343546ade658b31020855fd
Himanshu4746 <himanshukumar5208@gmail.com>
2016-10-15 19:31:34 +0530

    initial commit

---
+++ basic-pluin.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* Plugin Name: SendX
+* Description: Basic WordPress Plugin for Customisation
+* Version:     1.0
+* Author:      SendX
+* Author URI:  https://himanshu4746.github.io/
+* License:     GPL2
+*/
+$name="";
+
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'SendX', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+
+	global $ACCID;
+	?>
+	<div>
+		<h1> sendx Settings </h1>
+		<a href="#" target="_blank">Take a Tour</a>
+		<a href="#" target="_blank">Support</a>
+		<div id="confirm"></div>
+		<div>
+			<form id="frm" action="">
+				Account ID <input type="text" name="acid" value=""><br>
+			</form>
+			<button onclick="myFunction()">Save Changes</button>
+
+			<div id="demo" style="display:none;">id:</div>
+
+			<script>
+				function myFunction() {
+					var x = document.getElementById("frm");
+					var text = "";
+					var i;
+					for (i = 0; i < x.length ;i++) {
+						text += x.elements[i].value;
+					}
+					//document.getElementById("demo").innerHTML = text;
+					//document.cookie=text;
+					if(text){
+					document.getElementById("confirm").innerHTML = "Settings Saved";
+					}
+				}
+			</script>
+		</div>
+		
+	</div>	
+	<?php
+}
+//alert
+
+function fxn_load_custom_script(){
+	$ACCID="A140";
+	//wp_register_style('custom_plugin_css',plugin_dir_url(__FILE__).ACCID.'/main.css',false,'0.1');
+	//wp_enqueue_style('custom_plugin_css');
+	wp_enqueue_script('custom_plugin_js',plugin_dir_url(__FILE__).$ACCID.'/script.js');
+}
+//add_action( 'admin_enqueue_scripts', 'fxn_load_custom_script');
+add_action( 'wp_enqueue_scripts', 'fxn_load_custom_script');
+?>
